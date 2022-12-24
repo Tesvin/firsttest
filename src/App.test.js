809a4b0a03fc7b1054f0ec1ee948e19cdc9aedd1
stephen azosike <stephenazosike@gmail.com>
2022-12-24 09:15:37 +0100

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const HeaderEl = screen.getByRole('heading');
+  expect(HeaderEl.textContent).toBe('Test Website');
 });
